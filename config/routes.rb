86dace5f820802377c9c4a25e666235e7991b416
Léo Brival <leobrival@MacBook-Pro-de-Leo.local>
2022-11-14 15:41:35 -0400

    Add User Controller & View

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/update'
+  get 'users/edit'
+  get 'users/destroy'
   devise_for :users
   get 'pages/index'
   root to: "pages#home"
