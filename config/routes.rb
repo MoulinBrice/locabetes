168f2d9b0e8d0a919146583a72d6b3a9e38be675
brice moulin <moulin_brice@hotmail.com>
2022-11-14 23:24:12 -0400

    debut index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/update'
-  get 'users/edit'
-  get 'users/destroy'
+  resources :users
   devise_for :users
-  get 'pages/index'
-  root to: "pages#home"
+  root to: "pages#index"
 end
