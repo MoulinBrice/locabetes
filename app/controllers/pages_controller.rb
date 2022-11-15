83cdd8c2c293026fdc85603a505ab48dbfe36d11
dimitri Elie <dimi972@yahoo.fr>
2022-11-15 08:56:01 +0100

    correctif page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,5 @@
 class PagesController < ApplicationController
   # comment
   def index
-    @pak = Pak.all
   end
-
-  def show
-    @pak = Pak.find(params[:id])
-  end
-
-  private
-
-
-def pak_params
-  params.require(:pak).permit(:name, :description, :pricing, :photo)
-end
 end
