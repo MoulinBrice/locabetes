3a6a458bc732e60cb7d81fdcb4812bde220738cf
Léo Brival <leobrival@MacBook-Pro-de-Leo.local>
2022-11-15 16:02:49 -0400

    Reset db/migrate/20221115143740_create_bookings.rb

--- db/migrate/20221115143740_create_bookings.rb
+++ db/migrate/20221115143740_create_bookings.rb
@@ -1,4 +1,4 @@
-class CreateBooking < ActiveRecord::Migration[7.0]
+class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
       t.references :user, null: false, foreign_key: true
