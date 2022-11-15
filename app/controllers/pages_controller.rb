317f63fb555f2edb14c09ceff0529fbe36486c6a
dimitri Elie <dimi972@yahoo.fr>
2022-11-15 08:24:48 +0100

    dimitri

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
   # comment
   def index
+    @pak = Pak.all
   end
+
+  def show
+    @pak = Pak.find(params[:id])
+  end
+
+  private
+
+
+def pak_params
+  params.require(:pak).permit(:name, :description, :pricing, :photo)
+end
 end
