68bb6114ce1e785873ae0c9d5371f2b689aee7db
brice moulin <moulin_brice@hotmail.com>
2022-11-15 15:57:26 -0400

    Debut codage controller USER

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_path
   end
 
   def update
@@ -20,5 +27,12 @@ class UsersController < ApplicationController
   def destroy
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :phone, :email, :password)
+  end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  devise_for :users
   resources :orders
   resources :users
-  devise_for :users
   resources :paks
 end
