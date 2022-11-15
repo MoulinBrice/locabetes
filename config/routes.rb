bda1c5fcf2fcf2b765643195fbc5759a7f0552a1
dimitri Elie <dimi972@yahoo.fr>
2022-11-15 08:44:48 +0100

    add resource routes pak

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'paks/index'
-  get 'paks/show'
-  get 'paks/new'
-  get 'paks/create'
-  get 'paks/update'
-  get 'paks/delete'
-  get 'paks/search'
-  get 'paks/edit'
   root to: "pages#index"
   resources :users
   devise_for :users
+
+  resources :paks
 end
