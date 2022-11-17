db69eb8c205a7703df6b9f679812fb6e71ace9dd
Axel TOURET <axelj.at@gmail.com>
2022-11-17 10:59:51 -0400

    changement de colonne proprietaire>owner_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_141157) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_17_142904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_141157) do
     t.datetime "updated_at", null: false
     t.integer "rating"
     t.text "address"
-    t.string "nom_propriétaire"
+    t.string "owner_name"
     t.string "titre"
     t.string "dure"
   end
