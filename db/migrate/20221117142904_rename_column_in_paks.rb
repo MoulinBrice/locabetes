bc164916d5d3325dc7731db07872211908241786
dimitri Elie <dimi972@yahoo.fr>
2022-11-18 11:24:31 -0400

    modif card

--- db/migrate/20221117142904_rename_column_in_paks.rb
+++ db/migrate/20221117142904_rename_column_in_paks.rb
@@ -1,6 +1,5 @@
 class RenameColumnInPaks < ActiveRecord::Migration[7.0]
   def change
-    rename_column :paks, :nom_propriétaire, :owner_name
+    rename_column :paks, :nom_proprietaire, :owner_name
   end
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -32,6 +32,11 @@ pack = Pak.new(name: "Pack4" ,description: description,pricing: 800)
 file = URI.open("https://res.cloudinary.com/dz6lwhhko/image/upload/v1668650653/0702330631285-web-tete_yeveig.jpg")
 pack.photo.attach(io: file, filename: "one_dunkey.jpg", content_type: "image/jpg")
 pack.save
+
+pack = Pak.new(name: "Pack5" ,description: description,pricing: 500)
+file = URI.open("https://res.cloudinary.com/dz6lwhhko/image/upload/v1668650653/0702330631285-web-tete_yeveig.jpg")
+pack.photo.attach(io: file, filename: "one_dunkey.jpg", content_type: "image/jpg")
+pack.save
 # if pack.photo.attached?
 #   pack.save
 # else
@@ -52,7 +57,6 @@ user4 = User.new(first_name: 'Leo', last_name: 'BRIVAL', email: 'leo@gmail.com',
 user4.save
 
 
-<<<<<<< HEAD
 booking1 = Booking.new(user_id: user1.id, pak_id: pack1.id,starts_at: Time.now, ends_at: Time.now.next_week)
 booking1.save
 booking2 = Booking.new(user_id: user2.id, pak_id: pack2.id,starts_at: Time.now, ends_at: Time.now.next_week)
@@ -61,14 +65,3 @@ booking3 = Booking.new(user_id: user3.id, pak_id: pack3.id,starts_at: Time.now, 
 booking3.save
 booking4 = Booking.new(user_id: user4.id, pak_id: pack4.id,starts_at: Time.now, ends_at: Time.now.next_week)
 booking4.save
-=======
-
-# booking1 = Booking.new(user_id: user1.id, pak_id: pack1.id,starts_at: Time.now, ends_at: Time.now.next_week)
-# booking1.save
-# booking2 = Booking.new(user_id: user2.id, pak_id: pack2.id,starts_at: Time.now, ends_at: Time.now.next_week)
-# booking2.save
-# booking3 = Booking.new(user_id: user3.id, pak_id: pack3.id,starts_at: Time.now, ends_at: Time.now.next_week)
-# booking3.save
-# booking4 = Booking.new(user_id: user4.id, pak_id: pack4.id,starts_at: Time.now, ends_at: Time.now.next_week)
-# booking4.save
->>>>>>> c36f6c822a978e4fd13c1817cc7aa3e2c0a71fef
